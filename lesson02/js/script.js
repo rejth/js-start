f271fd71fa323eaf0f2f92bf2493569fd6dbbb49
Ilia Kirsanov <kirsanovia@yandex.ru>
2020-11-21 11:31:18 +0300

    finished lesson 2

---
+++ lesson02/js/script.js
@@ -0,0 +1,20 @@
+let money = 150000;
+console.log('Type of money: ', typeof(money));
+
+let income = '20000';
+console.log('Type of income: ', typeof(income));
+
+let addExpenses = 'ИНТЕРНЕТ, КОММУНАЛКА, СВЯЗЬ';
+console.log('Length of addExpenses: ', addExpenses.length);
+console.log('addExpenses list: ', addExpenses.toLowerCase().split(','));
+
+let deposit = true;
+console.log('Type of deposit: ', typeof(deposit));
+
+let mission = 700000;
+let period = 6;
+console.log('Период равен', period, 'месяцев');
+console.log('Цель заработать', mission, 'рублей');
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
