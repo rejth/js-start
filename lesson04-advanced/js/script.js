e0b287eda32954f5dc6149bd95a94d27a36f7e29
Ilia Kirsanov <kirsanovia@yandex.ru>
2020-11-24 22:59:02 +0300

    finished lesson 3 advanced

--- lesson04-advanced/js/script.js
+++ lesson04-advanced/js/script.js
@@ -4,7 +4,7 @@ function replaceString(argument) {
   if (typeof(argument) !== 'string') {
     return 'Введите строку, пожалуйста!';
   } else if (argument.length > 30) {
-    return argument.substring(0, 31) + '...';
+    return argument.substring(0, 30) + '...';
   } else {
     return argument;
   }
