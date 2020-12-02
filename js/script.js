36d811715be9c6f79b2f400495e7409d04a044ab
Ilia Kirsanov <kirsanovia@yandex.ru>
2020-12-02 22:43:18 +0300

    finished lesson 10

--- js/script.js
+++ js/script.js
@@ -1,85 +1,40 @@
 'use strict';
 
-// Кнопку "Рассчитать" через id
-const calcButton = document.getElementById('start');
-console.log('calcButton: ', calcButton);
+// Восстановить порядок книг.
+const books = document.querySelectorAll('.book');
+console.log('books: ', books);
 
-// Кнопки “+” (плюс) через Tag, каждую в своей переменной.
-const buttonPlus1 = document.getElementsByTagName('button')[0];
-console.log('buttonPlus1: ', buttonPlus1);
-const buttonPlus2 = document.getElementsByTagName('button')[1];
-console.log('buttonPlus2: ', buttonPlus2);
+books[0].before(books[1]);
+books[2].before(books[4]);
+books[2].before(books[3]);
+books[2].before(books[5]);
 
-// Чекбокс по id через querySelector
-const checkbox = document.querySelector('#deposit-check');
-console.log('checkbox: ', checkbox);
+// Заменить картинку заднего фона на другую из папки image
+document.body.style.backgroundImage = 'url(image/you-dont-know-js.jpg)';
 
-// Поля для ввода возможных доходов (additional_income-item) при помощи querySelectorAll
-const income1 = document.querySelectorAll('.additional_income-item')[0];
-console.log('income1: ', income1);
-const income2 = document.querySelectorAll('.additional_income-item')[1];
-console.log('income2: ', income2);
+// Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
+const bookTitle3 = document.querySelectorAll('.book a')[2];
+console.log('bookTitle3: ', bookTitle3);
+bookTitle3.textContent = 'Книга 3. this и Прототипы Объектов';
 
-// Каждый элемент в правой части программы через класс(не через querySelector), которые имеют в имени класса "-value", начиная с class="budget_day-value" и заканчивая class="target_month-value">
-const budgetDay = document.getElementsByClassName('budget_day-value')[0];
-console.log('budgetDay: ', budgetDay);
+// Удалить рекламу со страницы
+const adv = document.querySelector('.adv').remove();
 
-const expensesMonth = document.getElementsByClassName('expenses_month-value')[0];
-console.log('expensesMonth: ', expensesMonth);
+// Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+const book2 = document.querySelectorAll('.book ul')[1].children;
+console.log('book2: ', book2);
+book2[3].after(book2[6]);
+book2[4].after(book2[8]);
 
-const additionalIncome = document.getElementsByClassName('additional_income-value')[0];
-console.log('additionalIncome: ', additionalIncome);
+const book5 = document.querySelectorAll('.book ul')[4].children;
+console.log('book5 : ', book5);
+book5[2].before(book5[9]);
+book5[2].after(book5[4]);
+book5[3].after(book5[5]);
 
-const additionalExpenses = document.getElementsByClassName('additional_expenses-value')[0];
-console.log('additionalExpenses: ', additionalExpenses);
+// в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+const newElement = document.createElement('li');
+newElement.textContent = 'Глава 8: За пределами ES6';
 
-const incomePeriod = document.getElementsByClassName('income_period-value')[0];
-console.log('incomePeriod: ', incomePeriod);
-
-const targetMonth = document.getElementsByClassName('target_month-value')[0];
-console.log('targetMonth: ', targetMonth);
-
-// Оставшиеся поля через querySelector каждый в отдельную переменную:
-// поля ввода (input) с левой стороны и не забудьте про range.
-const inputs = document.querySelectorAll('.data input');
-console.log('inputs : ', inputs );
-inputs.forEach(element => console.log(element));
-
-const salaryAmount = inputs[0];
-console.log('salaryAmount: ', salaryAmount);
-
-const incomeTitle = inputs[1];
-console.log('incomeTitle: ', incomeTitle);
-
-const incomeAmount = inputs[2];
-console.log('incomeAmount: ', incomeAmount);
-
-const additionalIncomeItem1 = inputs[3];
-console.log('additionalIncomeItem1: ', additionalIncomeItem1);
-
-const additionalIncomeItem2 = inputs[4];
-console.log('additionalIncomeItem2: ', additionalIncomeItem2);
-
-const expensesTitle = inputs[5];
-console.log('expensesTitle: ', expensesTitle);
-
-const expensesAmount = inputs[6];
-console.log('expensesAmount: ', expensesAmount);
-
-const additionalExpensesItem = inputs[7];
-console.log('additionalExpensesItem : ', additionalExpensesItem);
-
-const depositCheck = inputs[8];
-console.log('depositCheck: ', depositCheck);
-
-const depositAmount = inputs[9];
-console.log('depositAmount : ', depositAmount );
-
-const depositPercent = inputs[10];
-console.log('depositPercent: ', depositPercent);
-
-const targetAmount = inputs[11];
-console.log('targetAmount: ', targetAmount);
-
-const periodSelect = inputs[12];
-console.log('periodSelect : ', periodSelect );
+const book6 = document.querySelectorAll('.book ul')[5];
+book6.append(newElement);
