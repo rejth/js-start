29cb1516cdf3fa3735191a7508770d8384d73eaf
Ilia Kirsanov <kirsanovia@yandex.ru>
2020-12-07 13:01:16 +0300

    fix advanced lesson 9

--- lesson09-advanced/js/script.js
+++ lesson09-advanced/js/script.js
@@ -11,49 +11,63 @@ const options = {
   second: 'numeric'
 };
 
-const addZeroBeforeNumber = function(day, month) {
-  if (day < 10) {day = '0' + day;}
-  if (month < 10) {month = '0' + month;}
-};
+function getNoun(number, wordOne, wordTwo, wordThree) {
+    let n = Math.abs(number);
+    n %= 100;
+    if (n >= 5 && n <= 20) {
+      return wordThree;
+    }
+    n %= 10;
+    if (n === 1) {
+      return wordOne;
+    }
+    if (n >= 2 && n <= 4) {
+      return wordTwo;
+    }
+    return wordThree;
+}
 
-const changeStringSpelling = function(hours) {
-  if (hours === 1 || hours === 21) {
-    hours = hours + ' час';
-  } else if ((hours > 2 && hours < 4) || (hours === 22 || hours === 23)) {
-    hours = hours + ' часа';
-  } else if ((hours > 5 && hours < 20) || hours === 0) {
-    hours = hours + ' часов';
-  }
-};
+function currentDateToFirstFormat(currentDateFormat, hours, minutes, seconds) {
+   // Вторник
+  currentDateFormat[0] = currentDateFormat[0].charAt(0).toLocaleUpperCase('RU') + currentDateFormat[0].slice(1);
+   // 4 февраля 2020
+  currentDateFormat[1] = currentDateFormat[1].slice(0, -3);
 
-const render = function() {
-  const now = new Date();
-  const nowFormat = now.toLocaleString("ru", options).split(', ');
-  let day = now.getDate(),
-      month = now.getMonth() + 1,
-      year = now.getFullYear(),
-      hours = now.getHours(),
-      minutes = now.getMinutes(),
-      seconds = now.getSeconds();
+  hours = (hours + getNoun(hours, ' час', ' часа', ' часов')).toString(); // 21 час
+  minutes = (minutes + getNoun(minutes, ' минута', ' минуты', ' минут')).toString(); // 5 минут
+  seconds = (seconds + getNoun(seconds, ' секунда', ' секунды', ' секунд')).toString(); // 33 секунды
 
-  addZeroBeforeNumber(day, month);
-  changeStringSpelling(hours);
-
-  nowFormat[0] = nowFormat[0].charAt(0).toLocaleUpperCase('RU') + nowFormat[0].slice(1); // Вторник
-  nowFormat[1] = nowFormat[1].slice(0, -3); // 4 февраля 2020
-
-  // Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды
-  const dateFormatOne = 'Сегодня ' + nowFormat[0] + ', ' + nowFormat[1] + ' года, ' +
+  // Формат - Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды
+  const dateFormatOne = 'Сегодня ' + currentDateFormat[0] + ', ' + currentDateFormat[1] + ' года, ' +
                         hours + ', ' + minutes + ', ' + seconds;
-
-  // 04.02.2020 - 21:05:33
-  const dateFormatTwo = day + '.' + month + '.' + year + ' - ' + nowFormat[2];
 
   let taskOne = document.querySelector('.task-1');
   taskOne.textContent = dateFormatOne;
+}
+
+function currentDateToSecondFormat(currentDateFormat, day, month, year) {
+  if (day < 10) {day = '0' + day;} // 04
+  if (month < 10) {month = '0' + month;} // 02
+
+  // Формат - 04.02.2020 - 21:05:33
+  const dateFormatTwo = day + '.' + month + '.' + year + ' - ' + currentDateFormat[2];
 
   let taskTwo = document.querySelector('.task-2');
   taskTwo.textContent = dateFormatTwo;
+}
+
+const render = function() {
+  const currentDate = new Date();
+  const currentDateFormat = currentDate.toLocaleString("ru", options).split(', ');
+  let day = currentDate.getDate(),
+      month = currentDate.getMonth() + 1,
+      year = currentDate.getFullYear(),
+      hours = currentDate.getHours(),
+      minutes = currentDate.getMinutes(),
+      seconds = currentDate.getSeconds();
+
+  currentDateToFirstFormat(currentDateFormat, hours, minutes, seconds);
+  currentDateToSecondFormat(currentDateFormat, day, month, year);
 };
 
 setInterval(function() {
