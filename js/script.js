883c4463d8d1705a06f8e0f1c10a93094a05a964
Ilia Kirsanov <kirsanovia@yandex.ru>
2020-12-14 22:10:10 +0300

    finished part 1 lesson 18

---
+++ js/script.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  function countTimer(deadline) {
+    const timerHours = document.querySelector('#timer-hours'),
+          timerMinutes = document.querySelector('#timer-minutes'),
+          timerSeconds = document.querySelector('#timer-seconds');
+
+    let idInterval;
+
+    function getTimeRemaining() {
+      const deadlineDate = new Date(deadline).getTime(),
+            currentDate = new Date().getTime(),
+            timeRemaining = (deadlineDate - currentDate) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor(timeRemaining / 3600);
+      return {timeRemaining, hours, minutes, seconds};
+    }
+
+    function updateClock() {
+      const timer = getTimeRemaining();
+
+      if (timer.timeRemaining < 0 || timer.timeRemaining === 0) {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+        clearInterval(idInterval);
+      } else {
+        timerHours.textContent = timer.hours < 10 ? '0' + timer.hours : timer.hours;
+        timerMinutes.textContent = timer.minutes < 10 ? '0' + timer.minutes : timer.minutes;
+        timerSeconds.textContent = timer.seconds < 10 ? '0' + timer.seconds : timer.seconds;
+      }
+    }
+
+    idInterval = setInterval(updateClock, 1000);
+  }
+
+  countTimer('17 december 2020');
+
+});
